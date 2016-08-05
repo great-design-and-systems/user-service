89166876452d068e57b10b659610ef6e29e66949
rickzx98 <rickzx98@gmail.com>
2016-08-06 02:50:32 +0800

    update api guide

--- src/boundary/user-resource.js
+++ src/boundary/user-resource.js
@@ -4,9 +4,9 @@ var NotFoundException = require('../control/not-found-exception');
 var getRegisterResponse = require('../control/get-register-response');
 var API = process.env.API_NAME || '/api/users/';
 
-module.exports = function(app) {
-    app.get(API + 'user-profile/:username', function(req, res) {
-        User.getUserProfileByUsername(req.params.username, function(err, result) {
+module.exports = function (app) {
+    app.get(API + 'user-profile/:username', function (req, res) {
+        User.getUserProfileByUsername(req.params.username, function (err, result) {
             if (err) {
                 res.status(404).send(new NotFoundException('User profile'));
             } else {
@@ -15,8 +15,8 @@ module.exports = function(app) {
         });
     });
 
-    app.get(API + 'user-password/:username', function(req, res) {
-        User.getUserPasswordByUsername(req.params.username, function(err, result) {
+    app.get(API + 'user-password/:username', function (req, res) {
+        User.getUserPasswordByUsername(req.params.username, function (err, result) {
             if (err) {
                 res.status(404).send(new NotFoundException('Password'));
             } else {
@@ -27,14 +27,14 @@ module.exports = function(app) {
         });
     });
 
-    app.post(API + 'register', function(req, res) {
-        User.register(req.body, function(err, result) {
+    app.post(API + 'register', function (req, res) {
+        User.register(req.body, function (err, result) {
             new getRegisterResponse(req, res, err, result);
         });
     });
 
-    app.delete(API + ':userId', function(req, res) {
-        User.removeUser(req.params.userId, function(err, result) {
+    app.delete(API + ':userId', function (req, res) {
+        User.removeUser(req.params.userId, function (err, result) {
             if (err) {
                 res.status(500).send({
                     message: 'Failed to remove user id ' + req.params.id + '.'
@@ -45,14 +45,26 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/', function(req, res) {
+    app.get('/', function (req, res) {
         res.status(200).send({
             domain: process.env.DOMAIN_NAME || 'User',
             links: {
-                register: 'http://' + req.headers.host + API + 'register/',
-                userProfile: 'http://' + req.headers.host + API + 'user-profile/{username}/',
-                userPassword: 'http://' + req.headers.host + API + 'user-password/{username}/',
-                DELETE: 'http://' + req.headers.host + API + '{userId}/'
+                register: {
+                    method: 'POST',
+                    url: 'http://' + req.headers.host + API + 'register/',
+                },
+                userProfile: {
+                    method: 'GET',
+                    url: 'http://' + req.headers.host + API + 'user-profile/:username'
+                },
+                userPassword: {
+                    method: 'GET',
+                    url: 'http://' + req.headers.host + API + 'user-password/:username'
+                },
+                deleteUser: {
+                    method: 'DELETE',
+                    url: 'http://' + req.headers.host + API + ':userId'
+                }
             }
         });
     });
