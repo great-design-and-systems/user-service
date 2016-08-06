5514467b1fcbff5683307866ea1ca1287a94eccc
rickzx98 <rickzx98@gmail.com>
2016-08-06 22:07:56 +0800

    updates

--- src/control/get-user-password-by-username.js
+++ src/control/get-user-password-by-username.js
@@ -3,12 +3,19 @@ var User = require('../entity/User');
 
 function execute(username, callback) {
     User.findOne({ username: username }, function (err, result) {
-        if (err) {
+        try {
+            if (err) {
+                callback({
+                    message: 'Password not found.'
+                });
+            } else {
+                callback(undefined, { password: result.password });
+            }
+        } catch (error) {
+            console.error(error);
             callback({
-                message: 'Password not found.'
+                message: 'Password not found'
             });
-        } else {
-            callback(undefined, { password: result.password });
         }
     });
 }
