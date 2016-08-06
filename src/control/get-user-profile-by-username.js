02f7f7a368029dd3f8bc2a58ee155a1ca89caba1
rickzx98 <rickzx98@gmail.com>
2016-08-06 16:02:58 +0800

    updates

--- src/control/get-user-profile-by-username.js
+++ src/control/get-user-profile-by-username.js
@@ -1,29 +1,36 @@
 'use strict';
 var UserProfile = require('../entity/User-profile');
 var User = require('../entity/User');
-
+var log4js = require('log4js');
+var logger = log4js.getLogger();
 function execute(username, callback) {
     User.findOne({
         username: username
-    }, function(err, result) {
+    }, function (err, result) {
         if (err) {
             callback({
                 message: 'User not found.'
             });
         } else {
-            UserProfile.findOne({
-                userId: result._id
-            }, function(err, userProfileResult) {
-                if (err) {
-                    callback({
-                        message: 'User profile not found'
-                    });
-                } else {
-                    userProfileResult._id = undefined;
-                    userProfileResult.userId = undefined;
-                    callback(undefined, userProfileResult);
-                }
-            });
+            try {
+                UserProfile.findOne({
+                    userId: result._id
+                }, function (err, userProfileResult) {
+                    if (err) {
+                        callback({
+                            message: 'User profile not found'
+                        });
+                    } else {
+                        userProfileResult._id = undefined;
+                        userProfileResult.userId = undefined;
+                        callback(undefined, userProfileResult);
+                    }
+                });
+            } catch (errProfile) {
+                callback({
+                    message: 'User profile not found'
+                });
+            }
         }
     });
 
